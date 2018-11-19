5960f08d07509d4ac3f7e142d73ec7c73e120d29
William Hilton <wmhilton@gmail.com>
2018-11-19 00:36:01 -0500

    work in node hopefully?

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  moduleNameMapper: {
+    "isomorphic-textencoder(.*)$": "<rootDir>/src/isomorphic-textencoder$1",
+    "@isomorphic-pgp/generate(.*)$": "<rootDir>/src/isomorphic-pgp/generate$1",
+    "@isomorphic-pgp/parser(.*)$": "<rootDir>/src/isomorphic-pgp/parser$1",
+    "@isomorphic-pgp/sign-and-verify(.*)$": "<rootDir>/src/isomorphic-pgp/sign-and-verify$1",
+    "@isomorphic-pgp/util(.*)$": "<rootDir>/src/isomorphic-pgp/util$1",
+    "select-case(.*)$": "<rootDir>/src/select-case$1"
+  }
+};
