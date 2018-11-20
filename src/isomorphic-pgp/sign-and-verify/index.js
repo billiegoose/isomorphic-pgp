c06422593a9fa5ebdeee6e3a104fee4ab02fde58
William Hilton <wmhilton@gmail.com>
2018-11-20 17:55:44 -0500

    add an index file so we can weight sign-and-verify

---
+++ src/isomorphic-pgp/sign-and-verify/index.js
@@ -0,0 +1,5 @@
+const { verifySelfSignature } = require('./verifySelfSignature.js')
+const { verify } = require('./verify.js')
+const { sign } = require('./sign.js')
+
+module.exports = { verifySelfSignature, verify, sign }
