be43ea76418d797b9d9ce001d10712287a6449b6
William Hilton <wmhilton@gmail.com>
2018-11-22 23:23:39 -0500

    Cave and add sha.js fallback for crypto-hash in sign-and-verify

--- src/isomorphic-pgp/sign-and-verify/hashes.js
+++ src/isomorphic-pgp/sign-and-verify/hashes.js
@@ -1,8 +1,18 @@
-const { sha1, sha256, sha384, sha512 } = require("crypto-hash");
+const { sha1, sha256, sha384, sha512 } = require('crypto-hash')
+const Hash = require('sha.js/sha1')
 
 module.exports.hashes = {
-    SHA1: sha1,
-    SHA256: sha256,
-    SHA384: sha384,
-    SHA512: sha512
-};
+  SHA1: async (buffer, options) => {
+    let result
+    try {
+      result = await sha1(buffer, options)
+    } catch (err) {
+      // IE fallback
+      result = new Hash().update(buffer).digest()
+    }
+    return result
+  },
+  SHA256: sha256,
+  SHA384: sha384,
+  SHA512: sha512
+}
