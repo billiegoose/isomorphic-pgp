c10a8be2aaf55ae2f5cdb83459297fb3ff2319ae
William Hilton <wmhilton@gmail.com>
2018-11-22 22:59:44 -0500

    use sha.js for fingerprint since SHA-1 is not available in Edge

--- src/isomorphic-pgp/util/fingerprint.js
+++ src/isomorphic-pgp/util/fingerprint.js
@@ -1,8 +1,8 @@
-const { sha1 } = require("crypto-hash");
 const PublicKey = require("@isomorphic-pgp/parser/Packet/PublicKey.js");
+const Hash = require("sha.js/sha1");
 
 module.exports.fingerprint = async function fingerprint(packet) {
   let buffer = await PublicKey.serializeForHash(packet);
-  let hash = await sha1(buffer, { outputFormat: "buffer" });
+  let hash = new Hash().update(buffer).digest();
   return new Uint8Array(hash);
 }
